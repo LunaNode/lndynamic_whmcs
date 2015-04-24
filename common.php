a6d56da13302a6435cc8431687e93cb9b0f119e7
Favyen Bastani <fbastani@perennate.com>
2015-04-23 22:18:21 -0400

    Initial commit (v0.1.1).

---
+++ common.php
@@ -0,0 +1,92 @@
+<?php
+
+function lunandoe_uid($length) {
+		$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+		$string = "";
+
+	for ($p = 0; $p < $length; $p++) {
+				$string .= $characters[mt_rand(0, strlen($characters) - 1)];
+		}
+
+		return $string;
+}
+
+function mysql_query_safe($query, array $params = array()) {
+    if (!empty($params)) {
+        // there is possibility to use % sign in query - this line escapes it!
+        $query = str_replace('%', '%%', $query);
+
+        foreach ($params as $k => $p) {
+            if ($p === null) {
+                $query = preg_replace('/\?/', 'NULL', $query, 1);
+                unset($params[$k]);
+            } elseif (is_int($p) || is_float($p)) {
+                $query = preg_replace('/\?/', $p, $query, 1);
+                unset($params[$k]);
+            } else {
+                $query = preg_replace('/\?/', "'%s'", $query, 1);
+            }
+        }
+        foreach ($params as &$v)
+            $v = mysql_real_escape_string($v);
+
+        $sql_query = vsprintf(str_replace("?", "'%s'", $query), $params);
+
+        $sql_query = mysql_query($sql_query);
+    } else {
+        $sql_query = mysql_query($query);
+    }
+
+    $err = mysql_error();
+    if (!$sql_query && $err) {
+        throw new Exception($err);
+    }
+    return ($sql_query);
+}
+
+function lunanode_customFieldExists($relid, $fieldname) {
+	$result = mysql_query_safe("SELECT COUNT(*) FROM tblcustomfields WHERE `relid` = ? AND `type` = 'product' AND `fieldname` LIKE ?", array($relid, (strpos($fieldname, '|') ? $fieldname . '|%' : $fieldname . '%')));
+	$row = mysql_fetch_array($result);
+	return $row[0] > 0;
+}
+
+function lunanode_customFieldSet($relid, $fieldname, $serviceid, $value) {
+    if(lunanode_customFieldExists($relid, $fieldname)) {
+        $result = mysql_query_safe("SELECT `id` FROM tblcustomfields WHERE `relid` = ? AND `type` = 'product' AND `fieldname` LIKE ?", array($relid, (strpos($fieldname, '|') ? $fieldname . '|%' : $fieldname . '%')));
+        $row = mysql_fetch_array($result);
+        mysql_query_safe('DELETE FROM tblcustomfieldsvalues WHERE `fieldid` = ? AND `relid` = ?', array($row[0], $serviceid));
+        mysql_query_safe('INSERT INTO tblcustomfieldsvalues (fieldid, relid, value) VALUES (?, ?, ?)', array($row[0], $serviceid, $value));
+    }
+}
+
+function lunanode_isActive($serviceid) {
+	$result = mysql_query_safe("SELECT `domainstatus` from tblhosting WHERE `id` = ?", array($serviceid));
+
+	if($row = mysql_fetch_array($result)) {
+		return $row[0] == 'Active';
+	} else {
+		return false;
+	}
+}
+
+function lunanode_redirect($url, $get = array(), $statusCode = 303) {
+	global $config;
+	$get_string = '';
+
+	foreach($get as $k => $v) {
+		if(!empty($get_string) || strpos($url, '?') !== false) {
+			$get_string .= '&';
+		} else {
+			$get_string .= '?';
+		}
+
+		$get_string .= urlencode($k);
+		$get_string .= '=';
+		$get_string .= urlencode($v);
+	}
+
+	header('Location: ' . $url . $get_string, true, $statusCode);
+	exit;
+}
+
+?>
---
+++ lndynamic.php
@@ -0,0 +1,355 @@
+<?php
+
+require_once("common.php");
+
+function lndynamic_API($api_id, $api_key, $category, $action, $params = array()) {
+	$url = "https://dynamic.lunanode.com/api.php";
+
+	$fields = array();
+	$fields['api_id'] = $api_id;
+	$fields['api_key'] = $api_key;
+	$fields['category'] = $category;
+	$fields['action'] = $action;
+
+	foreach($params as $key => $value) {
+		$fields[urlencode($key)] = urlencode($value);
+	}
+
+	$fields_string = "";
+	foreach($fields as $key=>$value) {
+		$fields_string .= $key . '=' . $value . '&';
+	}
+	rtrim($fields_string, '&');
+
+	//open connection
+	$ch = curl_init();
+
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	if(!empty($fields)) {
+		curl_setopt($ch, CURLOPT_POST, count($fields));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+	}
+
+	//execute post
+	$raw = curl_exec($ch);
+
+	//close connection
+	curl_close($ch);
+
+	$result = json_decode($raw, true);
+
+	if(!is_array($result)) {
+		return array('error' => $raw);
+	} else {
+		return $result;
+	}
+}
+
+function lndynamic_ConfigOptions() {
+	return array(
+		"Plan name" => array("Type" => "dropdown", "Options" => "512 MB,1024 MB,2048 MB,4096 MB,8192 MB,16384 MB"),
+		"plan_id" => array("Type" => "text", "Size" => "5", "Description" => "Only required for special plans; if set, overrides plan name"),
+		"API id" => array("Type" => "text", "Size" => "20", "Description" => "Generate from API tab"),
+		"API key" => array("Type" => "text", "Size" => "30", "Description" => "Generate from API tab")
+	);
+}
+
+function lndynamic_CreateAccount($params) {
+	$domain = $params["domain"];
+	$configoptions = $params["configoptions"];
+
+	$plan_name = $params['configoption1'];
+	$plan_id = $params['configoption2'];
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(!lunanode_customFieldExists($params['pid'], 'vmid')) {
+		return 'Custom field vmid has not been configured.';
+	}
+
+	if(!empty($params['customfields']['vmid'])){
+		$result = lndynamic_API($api_id, $api_key, 'vm', 'info', array('vm_id' => $params['customfields']['vmid']));
+		if(!isset($result['error'])){
+			return 'Virtual machine already exists, please delete and try again.';
+		}
+	}
+	
+	if(!isset($configoptions['Operating System'])) {
+		return "Error: you must select an operating system!";
+	}
+	$os = $configoptions['Operating System'];
+
+	if(empty($api_id) || empty($api_key)) {
+		return "Error: product misconfiguration (backend interface not set).";
+	}
+
+	if(empty($plan_id)) {
+		if($plan_name == '1024 MB') {
+			$plan_id = 2;
+		} else if($plan_name == '2048 MB') {
+			$plan_id = 3;
+		} else if($plan_name == '4096 MB') {
+			$plan_id = 4;
+		} else if($plan_name == '8192 MB') {
+			$plan_id = 5;
+		} else if($plan_name == '16384 MB') {
+			$plan_id = 6;
+		} else {
+			$plan_id = 1;
+		}
+	} else {
+		$plan_id = intval($plan_id);
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'create', array('hostname' => $domain, 'plan_id' => $plan_id, 'image_id' => $os, 'wait' => 1));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	} else {
+		lunanode_customFieldSet($params['pid'], 'vmid', $params['serviceid'], $result['vm_id']);
+		return "success";
+	}
+}
+
+function lndynamic_TerminateAccount($params) {
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'delete', array('vm_id' => $params['customfields']['vmid']));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	} else {
+		lunanode_customFieldSet($params['pid'], 'vmid', $params['serviceid'], '');
+	}
+	
+	return "success";
+
+}
+
+function lndynamic_SuspendAccount($params) {
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'stop', array('vm_id' => $params['customfields']['vmid']));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	} else {
+		return "success";
+	}
+}
+
+function lndynamic_UnsuspendAccount($params) {
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	lndynamic_API($api_id, $api_key, 'vm', 'start', array('vm_id' => $params['customfields']['vmid']));
+	return "success";
+}
+
+function lndynamic_ChangePackage($params) {
+	return "Error: operation not supported.";
+}
+
+function lndynamic_ClientArea($params) {
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	$vmid = $params['customfields']['vmid'];
+	$info = lndynamic_API($api_id, $api_key, 'vm', 'info', array('vm_id' => $vmid));
+	$images = lndynamic_API($api_id, $api_key, 'image', 'list', array('vm_id' => $vmid));
+
+	if(!isset($info['info']) || !isset($images['images'])) {
+		return "Backend call failed.";
+	}
+
+	$extra = $info['extra'];
+	$info = $info['info'];
+	$images = $images['images'];
+
+	ob_start();
+	include(dirname(__FILE__) . "/template.php");
+	return ob_get_clean();
+}
+
+function lndynamic_AdminLink($params) {
+
+	$code = '<form action=\"http://'.$params["serverip"].'/controlpanel" method="post" target="_blank">
+<input type="hidden" name="user" value="'.$params["serverusername"].'" />
+<input type="hidden" name="pass" value="'.$params["serverpassword"].'" />
+<input type="submit" value="Login to Control Panel" />
+</form>';
+	return $code;
+
+}
+
+function lndynamic_LoginLink($params) {
+
+	echo "<a href=\"http://".$params["serverip"]."/controlpanel?gotousername=".$params["username"]."\" target=\"_blank\" style=\"color:#cc0000\">login to control panel</a>";
+
+}
+
+function lndynamic_action($params, $action, $extra = array()) {
+	if(!lunanode_isActive($params['serviceid'])) {
+		return 'Error: service is not currently active.';
+	}
+	
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	$args = array_merge(array('vm_id' => $params['customfields']['vmid']), $extra);
+	$result = lndynamic_API($api_id, $api_key, 'vm', $action, $args);
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	} else {
+		return "success";
+	}
+}
+
+function lndynamic_start($params) {
+	return lndynamic_action($params, 'start');
+}
+
+function lndynamic_reboot($params) {
+	return lndynamic_action($params, 'reboot');
+}
+
+function lndynamic_stop($params) {
+	return lndynamic_action($params, 'stop');
+}
+
+function lndynamic_rescue($params) {
+	return lndynamic_action($params, 'rescue');
+}
+
+function lndynamic_reimage($params) {
+	if(!isset($_REQUEST['os'])) {
+		return 'No operating system specified.';
+	}
+
+	if(!lunanode_isActive($params['serviceid'])) {
+		return 'Error: service is not currently active.';
+	}
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+	
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+	
+	$info = lndynamic_API($api_id, $api_key, 'vm', 'info', array('vm_id' => $params['customfields']['vmid']));
+
+	if(isset($info['error'])) {
+		return "Error: {$info['error']}.";
+	}
+
+	if(empty($info['info']['ip'])) {
+		lndynamic_API($api_id, $api_key, 'vm', 'floatingip-add', array('vm_id' => $params['customfields']['vmid'])); //maybe this failed to acquire IP, so try now
+		return "Error: VM does not have an IP address yet!";
+	} else if(empty($info['info']['hostname']) || empty($info['extra']['plan_id'])) {
+		return "Error: VM missing hostname attribtue.";
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'floatingip-delete', array('vm_id' => $params['customfields']['vmid'], 'keep' => 'yes'));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'delete', array('vm_id' => $params['customfields']['vmid']));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'create', array('hostname' => $info['info']['hostname'], 'plan_id' => $info['extra']['plan_id'], 'image_id' => $_REQUEST['os'], 'ip' => $info['info']['ip']));
+
+	if(isset($result['error'])) {
+		return "Error: {$result['error']}.";
+	}
+
+	lunanode_customFieldSet($params['pid'], 'vmid', $params['serviceid'], $result['vm_id']);
+	lunanode_redirect('clientarea.php', array('action' => 'productdetails', 'id' => $params['serviceid']));
+}
+
+function lndynamic_vnc($params) {
+	if(!lunanode_isActive($params['serviceid'])) {
+		return 'Error: service is not currently active.';
+	}
+	
+	$api_id = $params['configoption3'];
+	$api_key = $params['configoption4'];
+
+	if(empty($params['customfields']['vmid'])) {
+		return 'Virtual machine does not exist.';
+	}
+
+	$result = lndynamic_API($api_id, $api_key, 'vm', 'vnc', array('vm_id' => $params['customfields']['vmid']));
+
+	if(isset($result['vnc_url'])) {
+		lunanode_redirect($result['vnc_url']);
+	} else {
+		return "Error: VNC connection failed.";
+	}
+}
+
+function lndynamic_ClientAreaCustomButtonArray() {
+	$buttonarray = array(
+	 "Reboot Server" => "reboot",
+	 "Start Server" => "start",
+	 "Shutdown Server" => "stop",
+	 "Re-image" => "reimage",
+	 "Rescue" => "rescue",
+	 "VNC" => "vnc"
+	);
+	return $buttonarray;
+}
+
+function lndynamic_AdminCustomButtonArray() {
+	$buttonarray = array(
+	 "Reboot Server" => "reboot",
+	 "Start Server" => "start",
+	 "Shutdown Server" => "stop"
+	);
+	return $buttonarray;
+}
+
+function lndynamic_UsageUpdate($params) {
+	return;
+}
+
+function lndynamic_AdminServicesTabFields($params) {
+	return array();
+}
+
+function lndynamic_AdminServicesTabFieldsSave($params) {
+	return;
+}
+
+?>
